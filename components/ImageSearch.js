e0d89fdf5b5f548dcaf4498ebec78225cba87e9b
elofg <elofg@kth.se>
2024-10-30 13:48:08 +0100

    gitignore

--- components/ImageSearch.js
+++ components/ImageSearch.js
@@ -2,13 +2,6 @@ import { useState } from 'react'
 import Image from 'next/image'
 import styles from '../styles/ImageSearch.module.css'
 
-const simulatedDataset = [
-  'https://img01.ztat.net/article/spp-media-p1/35af95bc713b3b5e81271efb768824cb/1729533d161c48a2accf176cec87c1d2.jpg?imwidth=400&filter=packshot',
-  'https://img01.ztat.net/article/spp-media-p1/cc59f7e79f34318b9cfdb27fd062c957/5368b0c9acc7477d9a6f2bc89c06437e.jpg?imwidth=400&filter=packshot',
-  'https://img01.ztat.net/article/spp-media-p1/b3f2aa34ae7436ed920a083677828756/d4ec3066b39942209806f027592afca8.jpg?imwidth=400&filter=packshot',
-  'https://img01.ztat.net/article/spp-media-p1/cf973985efe5393084ec563850138123/72dcb49e47784eb7a37d2f1a452ab080.jpg?imwidth=400&filter=packshot'
-]
-
 export default function ImageSearch() {
   const [selectedFile, setSelectedFile] = useState(null)
   const [results, setResults] = useState([])
@@ -27,7 +20,7 @@ export default function ImageSearch() {
     formData.append('file', selectedFile)
 
     try {
-      const response = await fetch('http://localhost:8000/api/search', {
+      const response = await fetch('http://web-production-40d79.up.railway.app/api/search', {
         method: 'POST',
         body: formData,
       })
